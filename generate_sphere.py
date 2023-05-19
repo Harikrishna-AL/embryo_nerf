4153095d993c9909a5b7b7f8fbbead9d9f4eeecf
Harikrishna Pillai <amenu4aie21034@am.students.amrita.edu>
2023-05-19 23:42:15 +0530

    dumping data to json

--- generate_sphere.py
+++ generate_sphere.py
@@ -1,5 +1,6 @@
 import pyvista as pv
 import numpy as np
+import json
 
 sphere = pv.Sphere(radius=1)
 plotter = pv.Plotter()
@@ -13,6 +14,10 @@ positions = [
     [0, -5, 0],   # bottom view
     [-5, 0, 0],   # left view
 ]
+def data_to_json(dict):
+    json_object = json.dumps(dict, indent=4)
+    with open("sphere_data.json", "a") as outfile:
+        outfile.write(json_object)
 # while True:
 for i, pos in enumerate(positions):
     # plotter.camera_position = pos
@@ -22,5 +27,18 @@ for i, pos in enumerate(positions):
     trans_matrix = camera.GetViewTransformMatrix()
     # trans_matrix = plotter.get_view_matrix()
     plotter.show(title=f"View {i+1}",screenshot=f"./generated_views/view{i+1}.png",interactive_update=True)
-    print(plotter.camera.position)
-    print(trans_matrix)
+    # print(plotter.camera.position)
+    # print(np.array(trans_matrix))
+    trans_matrix_list = []
+    for i in range(4):
+        for j in range(4):
+            trans_matrix_list.append(trans_matrix.GetElement(i, j))
+    # trans_matrix_np = np.array(trans_matrix.GetElement(0,0))
+    data_to_json(trans_matrix_list)
+
+# dictionary = {
+#     "name": "sathiyajith",
+#     "rollno": 56,
+#     "cgpa": 8.6,
+#     "phonenumber": "9976770500"
+# }
