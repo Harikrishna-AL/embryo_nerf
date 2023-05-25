ac1a67d7a18a068d10fd30aba59b4f3d857cfe7c
Harikrishna <harikrishna@pop-os.localdomain>
2023-05-25 22:47:50 +0530

    edit: made the training loop

--- generate_sphere.py
+++ generate_sphere.py
@@ -18,25 +18,19 @@ def data_to_json(dict):
     json_object = json.dumps(dict, indent=4)
     with open("sphere_data.json", "a") as outfile:
         outfile.write(json_object + ",\n")
-# while True:
+
 def arr_to_3by3(arr):
     return np.array([[arr[0], arr[1], arr[2]], [arr[4], arr[5], arr[6]], [arr[8], arr[9], arr[10]]])
 
 for i, pos in enumerate(positions):
-    # plotter.camera_position = pos
     plotter.camera.position = pos
-    # plotter.camera_set = True
     camera = plotter.camera
     trans_matrix = camera.GetViewTransformMatrix()
-    # trans_matrix = plotter.get_view_matrix()
     plotter.show(title=f"View {i+1}",screenshot=f"./generated_views/view{i+1}.png",interactive_update=True)
-    # print(plotter.camera.position)
-    # print(np.array(trans_matrix))
     trans_matrix_list = []
     for i in range(4):
         for j in range(4):
             trans_matrix_list.append(trans_matrix.GetElement(i, j))
-    # trans_matrix_np = np.array(trans_matrix.GetElement(0,0))
     trans_matrix_list = arr_to_3by3(trans_matrix_list)
     data_to_json(trans_matrix_list.tolist())
 
--- nano_nerf/train.py
+++ nano_nerf/train.py
@@ -1,3 +1,4 @@
+from utils import compute_rays, compute_query_points, encoding_fun
 def train_iter(height,
                width,
                focal_len,
@@ -7,4 +8,15 @@ def train_iter(height,
                samples,
                encoding_fun,
                model):
-  pass
+  ray_directions, ray_origins = compute_rays(height,
+  width,
+  focal_len,
+  cam2world)
+  query_points, depth_values = compute_query_points(
+      ray_origins,
+      ray_directions,
+      near,
+      far,
+      samples)
+  query_points = query_points.reshape((-1,3))
+  encoded_points = encoding_fun(query_points)
