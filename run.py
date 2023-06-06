cd0111a42bfb9399f016471cb5c39a4f4a884f83
Harikrishna-AL <amenu4aie21034@am.students.amrita.edu>
2023-06-06 21:44:15 +0530

    update: run.py

--- run.py
+++ run.py
@@ -1,7 +1,17 @@
 from nano_nerf import nerf
-
-def train_nerf(dataloader):
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+def train_nerf(dataloader,epochs):
     model = nerf.NeRF()
-    
+    optimizer = torch.optim.Adam(model.parameters(),lr=0.001)
+    criterion = nn.MSELoss()
+    for epoch in range(epochs):
+        optimizer.zero_grad()
+        '''
+        to implement the rest
+        '''
+    pass
+
 if __name__ == "main":
-    train_nerf
+    train_nerf()
