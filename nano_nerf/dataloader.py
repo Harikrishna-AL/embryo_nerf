798a04a0ce0765e98bb20e8fd852e0ff17f86dd3
Harikrishna-AL <amenu4aie21034@am.students.amrita.edu>
2023-06-08 22:26:24 +0530

    add: dataloader.py

---
+++ nano_nerf/dataloader.py
@@ -0,0 +1,9 @@
+from torch.utils.data import Dataset, DataLoader
+
+class DatasetPro(Dataset):
+    def __init__(self,folder,transform=None):
+        pass
+    def __len__(self):
+        pass
+    def __getitem__(self,i):
+        pass
